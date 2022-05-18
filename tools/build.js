00e86f48c57bc8e5b516b3c74391f4b79ab83fd0
acarioni <alessandro.carioni@lightstreamer.com>
2022-05-18 10:50:40 +0200

    fix web worker compatibility

--- tools/build.js
+++ tools/build.js
@@ -27,8 +27,8 @@ const argv = require('minimist')(process.argv.slice(2), {
         'format': ['esm', 'cjs', 'cjs_min', 'umd', 'umd_min'],
         'config': 'build.config.js'}});
 
-const defaultWebVersion = '8.0.4';
-const defaultWebBuild = '1803';
+const defaultWebVersion = '8.0.5';
+const defaultWebBuild = '1805';
 const defaultNodeVersion = '8.0.4';
 const defaultNodeBuild = '1790';
 
@@ -38,7 +38,7 @@ config.webBuild = argv.build ? argv.build : defaultWebBuild;
 config.nodeVersion = argv.version ? argv.version : defaultNodeVersion;
 config.nodeBuild = argv.build ? argv.build : defaultNodeBuild;
 
-const defaultWebCid = 'pcYgxn8m8%20feOojyA1U661j3g2.pz47Af93nBwCvsb'
+const defaultWebCid = 'pcYgxn8m8%20feOojyA1U661k3g2.pz47AfB3nBwCvse'
 const defaultNodeCid = 'tqGko0tg4pkpW3DAK3Q4hwLri8LBV74eXyyz3u'
 const webCid = argv.LS_web_cid ? argv.LS_web_cid : defaultWebCid
 const nodeCid = argv.LS_node_cid ? argv.LS_node_cid : defaultNodeCid
@@ -395,11 +395,22 @@ function umdFooter(exportVar) {
         ${modules.map(m => `exports['${m}'] = ${exportVar}['${m}'];`).join('\n')}
     }
     else {
-        var namespace = createNs(extractNs(), window);
+        var namespace = createNs(extractNs(), lsGlobalObject());
         ${modules.map(m => `namespace['${m}'] = ${exportVar}['${m}'];`).join('\n')}
+    }
+
+    function lsIsBrowser() {
+        return typeof(window) !== 'undefined' && typeof(window.document) !== 'undefined';
+    }
+
+    function lsGlobalObject() {
+        return lsIsBrowser() ? window : self;
     }
     
     function extractNs() {
+        if (!lsIsBrowser()) {
+            return null;
+        }
         var scripts = window.document.getElementsByTagName("script");
         for (var i = 0, len = scripts.length; i < len; i++) {
             if ('data-lightstreamer-ns' in scripts[i].attributes) {        
