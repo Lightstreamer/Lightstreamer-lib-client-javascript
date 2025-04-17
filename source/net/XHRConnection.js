88b5dc7545796aac4d2d251c999d241202950975
acarioni <alessandro.carioni@lightstreamer.com>
2025-04-17 10:33:57 +0200

    Add copyright notice

--- source/net/XHRConnection.js
+++ source/net/XHRConnection.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2012 Lightstreamer Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 //see (the name there are obsolete, but the flow is quite the same):
 //https://docs.google.com/a/lightstreamer.com/drawings/d/1AUup89gqDhESi5j94gLsk7rS0T_FUSZ6hsbs1SCy_rM/edit?hl=en
 
